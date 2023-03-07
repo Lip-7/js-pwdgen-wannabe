df4e6face1ab9b28c3e5833fbf5c7089c7fbe8e5
Lip-7 <zhighofficial@gmail.com>
2023-03-07 14:59:22 +0100

    almost done

--- js/script.js
+++ js/script.js
@@ -6,11 +6,35 @@ function textCopy(){
 function getRandomInt(max){
     return Math.floor(Math.random() * max);
 }
+function inputCheck(value){
+    if (value){
+        if (isNaN(parseInt(value))){
+            return true
+        }else{
+            return false
+        }
+    }else{
+        return false
+    }
+};
 
 function passwordGenerating(){
     var userName = prompt("Please insert your first name");
-    var userLName = prompt("Please insert your last name");
-    var userColor = prompt("Please insert your favorite color");
-    let randomNumber = getRandomInt(1000)
-    document.getElementById("password").innerHTML = userName+userLName+userColor+randomNumber
+    let errorMessage = "Ma che stai a scrive??"
+    if (inputCheck(userName) == true){
+        var userLName = prompt("Please insert your last name");
+        if (inputCheck(userLName) == true){
+            var userColor = prompt("Please insert your favorite color");
+            if (inputCheck(userColor) == true){
+                let randomNumber = getRandomInt(1000)
+                document.getElementById("password").innerHTML = userName+userLName+userColor+randomNumber
+            }else{
+                alert(errorMessage)
+            }
+        }else{
+            alert(errorMessage)
+        }
+    }else{
+        alert(errorMessage)
+    }
 };
