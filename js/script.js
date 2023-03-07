29cabb03e378dff1cfc6ae65a02e0f32ac4e13ba
Lip-7 <zhighofficial@gmail.com>
2023-03-07 14:47:16 +0100

    init scripts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-function textCopy() {
+function textCopy(){
     var text = document.getElementById("password").innerText;
     navigator.clipboard.writeText(text);
     alert("text copied: " + text);
-  }
+}
+function getRandomInt(max){
+    return Math.floor(Math.random() * max);
+}
+
+function passwordGenerating(){
+    var userName = prompt("Please insert your first name");
+    var userLName = prompt("Please insert your last name");
+    var userColor = prompt("Please insert your favorite color");
+    let randomNumber = getRandomInt(1000)
+    document.getElementById("password").innerHTML = userName+userLName+userColor+randomNumber
+};
